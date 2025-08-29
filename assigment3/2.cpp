3c82fff221c37fe8e4456bbb57c9a460f1155f4c
arpit-svg <arpitkadyan292@gmail.com>
2025-08-29 15:00:21 +0530

    Create 2.cpp

---
+++ assigment3/2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    string str = "arpit";
+    stack<char> s;
+
+    for (char c : str) s.push(c);
+    cout << "Reversed";
+    while (!s.empty()) {
+        cout << s.top();
+        s.pop();
+    }
+}
